c4212342b84c9433271d0a36d640e7f58d235f7c
Mi Yeon Peach <peachkat@uw.edu>
2019-07-03 12:42:40 -0700

    fixed problem with button with help of Evan Feenstra

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import React from 'react';
 import './App.css';
 import coolpic from './staycool.png';
 import TextInput from './TextInput';
+import { FiMessageSquare } from 'react-icons/fi';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={coolpic} className="logo" alt="logo" />
-        <b>ChatApp</b>
-      </header>
-      <TextInput />
-    </div>
-  );
+  state={
+    messages:[]
+  }
+
+  gotMessage=(m)=>{
+    var messages = [...this.state.messages, m]
+    this.setState({messages})
+  }
+
+  render() {
+    var { messages } = this.state
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={coolpic} className="logo" alt="logo" />
+          <b>ChatApp</b>
+        </header>
+        <main className="messages">
+          {messages.map((m, i) => {
+            return <div key={i}>{m}</div>
+          })}
+        </main>
+        <TextInput sendMessage={this.gotMessage} />
+      </div>
+    )
+  }
 }
 
 export default App
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { FiSend } from 'react-icons/fi';
 
 class TextInput extends React.Component {
+
+    state={
+        text: ""
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({text: ""})
+    }
+
+    keyPress=(e)=>{
+        if(e.key==='Enter'){
+            this.send()
+        }
+    }
+
     render(){
         return(<div className="text-input">
-            <input />
-            <button><b>SEND</b></button>
+            <input value={this.state.text}
+                onChange={e=> this.setState({text: e.target.value})}
+                onKeyPress={this.keyPress}
+            />
+            <button disabled={!this.state.text} onClick={this.send}>
+                <FiSend style={{height:15,width:15}} />
+            </button>
             </div>)
     }
 }
