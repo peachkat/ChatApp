ca92c87a96a98284fab2df1ae85a949d7c61e250
Mi Yeon Peach <peachkat@uw.edu>
2019-07-17 11:35:38 -0700

    Added delete function so only Mimi can delete messages

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends React.Component {
 
   componentWillMount() {
     var name = localStorage.getItem('name')
-    if(name){
-      this.setState({name})
+    if (name) {
+      this.setState({ name })
     }
     /* <=========================> */
     firebase.initializeApp({
@@ -36,18 +36,29 @@ class App extends React.Component {
       snapshot.docChanges().forEach((change) => {
         if (change.type === "added") {
           //console.log(change.doc.data())
-          this.receive(change.doc.data())
+          this.receive({
+            ...change.doc.data(),
+            id: change.doc.id
+          })
+        }
+        if (change.type === 'removed') {
+          this.remove(change.doc.id)
         }
       })
     })
     /* <=========================> */
-  }
+  } //end componentWillMount
 
+  remove = (id) => {
+    var msgs = [...this.state.messages]
+    var messages = msgs.filter(m=> m.id!==id)
+    this.setState({messages})
+  }
   /* <===========================> */
   receive = (m) => {
     const messages = [m, ...this.state.messages]
     messages.sort((a, b) => b.ts - a.ts)
-    this.setState({messages})
+    this.setState({ messages })
   }
 
   send = (m) => {
@@ -63,61 +74,68 @@ class App extends React.Component {
     if (!editName) {
       localStorage.setItem('name', this.state.name)
     }
-    this.setState({editName})
+    this.setState({ editName })
   }
 
   takePicture = async (img) => {
-    this.setState({showCamera:false})
+    this.setState({ showCamera: false })
     const imgID = Math.random().toString(36).substring(7);
     var storageRef = firebase.storage().ref();
-    var ref = storageRef.child(imgID+'.jpg');
+    var ref = storageRef.child(imgID + '.jpg');
     await ref.putString(img, 'data_url')
-    this.send({img: imgID})
+    this.send({ img: imgID })
   }
 
   render() {
-    var {editName, messages, name, showCamera} = this.state
+    var { editName, messages, name, showCamera } = this.state
     return (
       <Div100vh className="App">
         {showCamera && <Camera takePicture={this.takePicture} />}
         <header className="header">
           <div>
             <img src={coolpic} className="logo" alt="logo" />
-            {editName ? '': 'ChatApp'}
+            {editName ? '' : 'ChatApp'}
           </div>
           <NamePicker
             name={name}
             editName={editName}
-            changeName={name => this.setState({name})}
+            changeName={name => this.setState({ name })}
             setEditName={this.setEditName}
           />
         </header>
         <main className="messages">
-          {messages.map((m,i)=>{
-            return <Message key={i} m={m} name={name} />
+          {messages.map((m, i) => {
+            return <Message key={i} m={m} name={name}
+              onClick={() => {
+                if(name==='Mimi') { //can also put if (name===m.from) to make only people who sent a message able to delete it
+                this.db.collection('messages').doc(m.id).delete()}
+                } //Now only 'Mimi' can delete messages >:3
+              }
+            />
           })}
         </main>
-        <TextInput sendMessage={text=> this.send({text})} 
-          showCamera={()=>this.setState({showCamera:true})}
-          />
+        <TextInput sendMessage={text => this.send({ text })}
+          showCamera={() => this.setState({ showCamera: true })}
+        />
       </Div100vh>
     )
-  }
-}
+  } //end render()
+} //end App component
 
 export default App;
 
-const bucket = 'first-project-1a6d0.appspot.com'
+const bucket = 'https://firebasestorage.googleapis.com/v0/b/first-project-1a6d0.appspot.com/o/'
 const suffix = '.jpg?alt=media'
 function Message(props) {
-  var{m, name} = props
+  var { m, name, onClick } = props
   return (<div className="bubble-wrap"
-    from={m.from===name ? "me" : "you"}
+    from={m.from === name ? "me" : "you"}
+    onClick={onClick}
   >
-    {m.from!==name && <div className="bubble-name">{m.from}</div>}
+    {m.from !== name && <div className="bubble-name">{m.from}</div>}
     <div className="bubble">
       <span>{m.text}</span>
-      {m.img && <img alt="pic" src={bucket+m.img+suffix} />}
+      {m.img && <img alt="pic" src={bucket + m.img + suffix} />}
     </div>
   </div>)
 }
